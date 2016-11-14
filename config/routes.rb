e4ccda18e80246f387c5e30035015c736942a57b
Aaron Ballard <mraballard@gmail.com>
2016-11-13 21:52:29 -0300

    added token authentication code to ApplicaitonController, added users_controller file and routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  resources :users, except: [:index] do
+    resources :order, except: [:update]
+  end
+  resources :store, only: [:create, :show]
 end
