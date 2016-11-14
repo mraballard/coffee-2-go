584455ca1124f88cfd2c5221932f0fe5f82c58a6
Aaron Ballard <mraballard@gmail.com>
2016-11-14 16:16:51 -0300

    added password_digest to users, user create route working with front end

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113183334) do
+ActiveRecord::Schema.define(version: 20161114191011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,8 +62,9 @@ ActiveRecord::Schema.define(version: 20161113183334) do
     t.string   "firstname"
     t.string   "lastname"
     t.string   "username"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
