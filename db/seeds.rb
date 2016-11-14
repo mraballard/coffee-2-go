42d3d23fd6a5faab21040b65713dca14f8ec1cb3
Aaron Ballard <mraballard@gmail.com>
2016-11-13 21:34:58 -0300

    added csv file and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'items.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+Store.create(name: "The Daily Grind")
+Menu.create(store_id: 1)
+
+csv.each do |row|
+  t = Item.new
+  t.name = row['name']
+  t.category = row['category']
+  t.description = row['description']
+  t.price = row['price']
+  t.menu_id = row['menu_id']
+
+  t.save
+  puts "#{t.errors}"
+end
+
+puts "There are now #{Item.count} rows in the items table"
