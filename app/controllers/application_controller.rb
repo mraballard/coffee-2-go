6f5a53e7395828e697c9d0f33f0fd8390ef7d016
Aaron Ballard <mraballard@gmail.com>
2017-07-03 09:18:55 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,22 @@
 class ApplicationController < ActionController::API
   def authenticate
-  render json: {status: 401, message: "unauthorized"} unless decode_token(bearer_token)
-end
+    render json: {status: 401, message: "unauthorized"} unless decode_token(bearer_token)
+  end
 
-def bearer_token
-  pattern = /^Bearer /
-  header  = request.env["HTTP_AUTHORIZATION"] # <= env
-  header.gsub(pattern, '') if header && header.match(pattern)
-end
+  def bearer_token
+    pattern = /^Bearer /
+    header  = request.env["HTTP_AUTHORIZATION"] # <= env
+    header.gsub(pattern, '') if header && header.match(pattern)
+  end
 
-def current_user
-  return if !bearer_token
-  decoded_jwt = decode_token(bearer_token)
-end
+  def current_user
+    return if !bearer_token
+    decoded_jwt = decode_token(bearer_token)
+  end
 
-def decode_token(token)
-  token = JWT.decode(token, nil, false) # this will error if the token is invalid or expired
-rescue
-  render json: {status: 401, message: 'invalid or expired token'}
-end
+  def decode_token(token)
+    token = JWT.decode(token, nil, false) # this will error if the token is invalid or expired
+  rescue
+    render json: {status: 401, message: 'invalid or expired token'}
+  end
 end
